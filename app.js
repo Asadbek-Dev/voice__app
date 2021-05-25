23a0d9322a9abef8e1b9c5cabd9077489554d745
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-05-25 23:56:30 +0500

    bug fixed

---
+++ app.js
@@ -0,0 +1,21 @@
+const btn=document.querySelector('.talk');
+const content=document.querySelector('.content');
+
+
+const SpeechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition=new SpeechRecognition();
+
+recognition.onstart=function(){
+    console.log('voice is activated, you can to microphoneeee');
+}
+
+recognition.onresult=function(event){
+    const current=event.resultIndex;
+
+    const transcript=event.results[current][0].transcript;
+    content.textContent=transcript;
+}
+
+btn.addEventListener('click',()=>{
+    recognition.start();
+})
