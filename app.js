26412df0ad836dd3f59228003596220f855002d4
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-05-26 00:28:58 +0500

    added special sentence

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const btn=document.querySelector('.talk');
 const content=document.querySelector('.content');
 
 
+const greetings=[
+    'Im good you little piece of love',
+    'Doing good homeboi',
+    'leave me alone'
+];
+const weather=['Weather is fine','You need a tan']
+
+
 const SpeechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition=new SpeechRecognition();
 
@@ -10,12 +18,30 @@ recognition.onstart=function(){
 }
 
 recognition.onresult=function(event){
+    console.log(event);
     const current=event.resultIndex;
-
     const transcript=event.results[current][0].transcript;
     content.textContent=transcript;
+    readOutLoud(transcript);
 }
 
 btn.addEventListener('click',()=>{
     recognition.start();
-})
+});
+
+function readOutLoud(message){
+    const speech = new SpeechSynthesisUtterance();
+    speech.text='i dont know what you said';
+
+    if(message.includes('how are you')){
+       const finalText = greetings[Math.floor(Math.random()*greetings.length)];
+        speech.text=finalText;
+    }
+
+
+    speech.volume=1;
+    speech.rate=1;
+    speech.pitch=1;
+
+    window.speechSynthesis.speak(speech);
+}
